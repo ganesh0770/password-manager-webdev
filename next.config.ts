ae4513845907a34d6d63f9ff1fb4ff93460dcd58
ganesh <ganeshkommu13@gmail.com>
2026-05-24 21:56:56 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Move it to the root level like this:
+  allowedDevOrigins: ['192.168.29.187'],
+
+  // Any other configs you have...
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
